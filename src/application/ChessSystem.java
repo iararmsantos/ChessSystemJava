b4154f7b04f8109804e839cb0e08e97ecfd8d43c
Iara Regina <iararmsantos@gmail.com>
2019-07-16 18:48:58 -0300

    Initial Project

---
+++ src/application/ChessSystem.java
@@ -0,0 +1,17 @@
+
+package application;
+
+/**
+ *
+ * @author Iara
+ */
+public class ChessSystem {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+    
+}
